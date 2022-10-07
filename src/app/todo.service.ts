98d44b406fea18df8271d479b71a3eed086bb774
ShawnCoots <cootsshawn@yahoo.com>
2022-10-06 20:27:39 -0500

    Added addNewTodo to service

--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -1,10 +1,19 @@
 import { Injectable } from '@angular/core';
+import { throwToolbarMixedModesError } from '@angular/material/toolbar';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TodoService {
   toDos: string[] = ['make stuff', 'get smarter', 'job time', 'profit!'];
+
+  addNewTodo(newTodo : string) {
+    this.toDos.push(newTodo);
+    return this.toDos;
+  }
+
+
+
 
   constructor() {}
 }
